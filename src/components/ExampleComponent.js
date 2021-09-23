93f72c3e8aa76519504dff33aaa10c44e2c68187
Jose Soto <jsoto7087@gmail.com>
2021-09-23 17:32:57 -0400

    Done.

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -5,6 +5,7 @@ function ExampleComponent() {
   return (
     <div className="example">
       <img
+        
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
@@ -12,5 +13,7 @@ function ExampleComponent() {
     </div>
   );
 }
+
+
 
 export default ExampleComponent;
